e8e5f9b964ddd672e1aee205d6fc3d118affbee8
Zev Ubu Hoffman <47284289+Zevubu@users.noreply.github.com>
2019-02-27 18:25:57 -0800

    next examle of work
    There is sadly a lot work and learning that went between each of my renditions, but none as drastic as between my first and second instances. This gap represent a at least a week or two of work if not more.

--- tarot.py
+++ tarot.py
@@ -1,3 +1,294 @@
+#Ask a question
+quest=input()
+max_index=len(quest)-1
+counter=0
+
+cat=[]  
+
+print (quest)
+
+while counter<=max_index:
+    bat=quest[counter]
+    if bat=='a':
+        art=1
+    elif bat=='b':
+        art=2
+    elif bat=='c' :
+        art=3
+    elif bat=='d':
+        art=4
+    elif bat=='e':
+        art=5
+    elif bat=='f':
+        art=6
+    elif bat=='g':
+        art=7
+    elif bat=='h':
+        art=8
+    elif bat=='i':
+        art=9
+    elif bat=='j':
+        art=10
+    elif bat=='k':
+         art=11
+    elif bat=='l':
+        art=12
+    elif bat=='m':
+        art=13
+    elif bat=='n':
+        art=14
+    elif bat=='o':
+        art=15
+    elif bat=='p':
+        art=16
+    elif bat=='q':
+        art=17
+    elif bat=='r':
+        art=18
+    elif bat=='s':
+        art=19
+    elif bat=='t':
+        art=20
+    elif bat=='u':
+        art=21
+    elif bat=='v':
+        art=22
+    elif bat=='w':
+        art=23
+    elif bat=='x':
+        art=24
+    elif bat=='y':
+        art=25
+    elif bat=='z':
+        art=26
+    elif bat=='A':
+        art=1
+    elif bat=='B':
+        art=2
+    elif bat=='C':
+        art=3
+    elif bat=='D':
+        art=4
+    elif bat=='E':
+        art=5
+    elif bat=='F':
+        art=6
+    elif bat=='G':
+        art=7
+    elif bat=='H':
+        art=8
+    elif bat=='I':
+        art=9
+    elif bat=='J':
+        art=10
+    elif bat=='K':
+        art=11
+    elif bat=='L':
+        art=12
+    elif bat=='M':
+        art=13
+    elif bat=='N':
+        art=14
+    elif bat=='O':
+        art=15
+    elif bat=='P':
+        art=16
+    elif bat=='Q':
+        art=17
+    elif bat=='R':
+        art=18
+    elif bat=='S':
+        art=19
+    elif bat=='T':
+        art=20
+    elif bat=='U':
+        art=21
+    elif bat=='V':
+        art=22
+    elif bat=='W':
+        art=23
+    elif bat=='X':
+        art=24
+    elif bat=='Y':
+        art=25
+    elif bat=='Z':
+        art=26
+    elif bat=='1':
+        art=1
+    elif bat=='2':
+        art=2
+    elif bat=='3' :
+        art=3
+    elif bat=='4':
+        art=4
+    elif bat=='5':
+        art=5
+    elif bat=='6':
+        art=6
+    elif bat=='7':
+        art=7
+    elif bat=='8':
+        art=8
+    elif bat=='9':
+        art=9
+    elif bat=='10':
+        art=10 
+    elif bat=='?':
+        art=23 
+    elif bat=='\'':
+        art=1  
+    else:
+        art=0
+    cat.append(art)
+    counter=counter+1
+    
+tom=len(cat)
+count=0
+til=0
+kit=cat[0]
+
+while tom>=til:
+    if (tom>=5 and cat[0]==23 and cat[1]==8 and cat[2]==5 and cat[3]==14 and cat[4]==0):
+        while count<=4:
+            cat.remove(cat[0])
+            count=count+1  
+    elif tom>=6 and cat[0]==23 and cat[1]==8 and cat[2]==5 and cat[3]==18 and cat[4]==5 and cat[5]==0:
+        while count<=5:
+            cat.remove(cat[0])
+            count=count+1    
+    elif tom>=6 and cat[0]==23 and cat[1]==8 and cat[2]==9 and cat[3]==12 and cat[4]==5 and cat[5]==0:
+        while count<=5:
+            cat.remove(cat[0])
+            count=count+1   
+    elif tom>=4 and cat[0]==23 and cat[1]==8 and cat[2]==25 and cat[3]==0:
+        while count<=3:
+            cat.remove(cat[0])
+            count=count+1    
+    elif tom>=5 and cat[0]==23 and cat[1]==8 and cat[2]==1 and cat[3]==20 and cat[4]==0:
+        while count<=4:
+            cat.remove(cat[0])
+            count=count+1                 
+    elif tom>=6 and cat[0]==23 and cat[1]==15 and cat[2]==21 and cat[3]==12 and cat[4]==4 and cat[5]==0:
+        while count<=5:
+            cat.remove(cat[0])
+            count=count+1
+    elif tom>=5 and cat[0]==23 and cat[1]==9 and cat[2]==12 and cat[3]==12 and cat[4]==0:
+        while count<=4:
+            cat.remove(cat[0])
+            count=count+1
+    elif tom>=7 and cat[0]==19 and cat[1]==8 and cat[2]==15 and cat[3]==21 and cat[4]==12 and cat[5]==4 and cat[6]==0:
+        while count<=6:
+            cat.remove(cat[0])
+            count=count+1
+    elif tom>=3 and cat[0]==9 and cat[1]==6 and cat[2]==0:
+         while count<=2:
+            cat.remove(cat[0])
+            count=count+1 
+    elif tom>=3 and cat[0]==9 and cat[1]==19 and cat[2]==0:
+        while count<=2:
+            cat.remove(cat[0])
+            count=count+1   
+    elif tom>=4 and cat[0]==3 and cat[1]==1 and cat[2]==14 and cat[3]==0:
+         while count<=3:
+             cat.remove(cat[0])
+             count=count+1 
+    elif tom>=6 and cat[0]==3 and cat[1]==15 and cat[2]==21 and cat[3]==12 and cat[4]==4 and cat[5]==0:
+         while count<=5:
+            cat.remove(cat[0])
+            count=count+1    
+    elif tom>=3 and cat[0]==4 and cat[1]==15 and cat[2]==0:
+        while count<=2:
+            cat.remove(cat[0])
+            count=count+1       
+    elif tom>=2 and cat[0]==9 and cat[1]==0:
+        while count<=1:
+            cat.remove(cat[0])   
+            count=count+1
+    elif tom>=4 and cat[0]==1 and cat[1]==18 and cat[2]==5 and cat[3]==0:
+        while count<=3:
+            cat.remove(cat[0])
+            count=count+1   
+    elif tom>=5 and cat[0]==23 and cat[1]==1 and cat[2]==14 and cat[3]==20 and cat[4]==0:
+        while count<=4:
+            cat.remove(cat[0]) 
+            count=count+1  
+    elif tom>=5 and cat[0]==23 and cat[1]==9 and cat[2]==19 and cat[3]==8 and cat[4]==0:
+        while count<=4:
+            cat.remove(cat[0]) 
+            count=count+1   
+    elif tom>=4 and cat[0]==23 and cat[1]==8 and cat[2]==15 and cat[3]==0:
+        while count<=3:
+            cat.remove(cat[0]) 
+            count=count+1
+    elif tom>=4 and cat[0]==8 and cat[1]==15 and cat[2]==23 and cat[3]==0:
+        while count<=3:
+            cat.remove(cat[0]) 
+            count=count+1  
+    elif tom>=6 and cat[0]==23 and cat[1]==8 and cat[2]==9 and cat[3]==3 and cat[4]==8 and cat[5]==0:
+        while count<=5:
+            cat.remove(cat[0])
+            count=count+1
+    elif tom>=6 and cat[0]==3 and cat[1]==1 and cat[2]==14 and cat[3]==1 and cat[4]==20 and cat[5]==0:
+        while count<=5:
+            cat.remove(cat[0])
+            count=count+1  
+    elif tom>=5 and cat[0]==3 and cat[1]==1 and cat[2]==14 and cat[3]==20 and cat[4]==0:
+        while count<=4:
+            cat.remove(cat[0])
+            count=count+1  
+    elif tom>=3 and cat[0]==1 and cat[1]==13 and cat[2]==0:
+         while count<=2:
+             cat.remove(cat[0])   
+             count=count+1    
+    til=til+1
+    count=0                                                     
+#done:when where while why what would will if is can should could do are want wish who how which can't cant am
+
+#to add:
+
+   
+  
+
+catindex=len(cat)-1
+count=0
+hat=(0) 
+
+while count<=catindex:
+      hat=hat+cat[count]
+      count=count+1
+
+import random
+
+cut=[]
+bil=2
+bui=0
+
+while bui<=bil:
+     x=random.randint(2,11)
+     if x not in cut:
+       bui=bui+1
+       cut.append(x)
+      
+thing1=cut[0] 
+thing2=cut[1]
+thing3=cut[2]
+
+tus=hat+thing1
+tut=hat+thing2
+tun=hat+thing3
+
+#cat== digets catindex== numberofdigets hat==total of all digets added thing1,2&3 == random diget between 2&11 tus,tut,tun==hat*thing1,2,3
+
+#print(cat)
+#print(catindex)
+print(hat)
+print(cut)   
+#print(thing1)
+#print(tus)
+#print(thing2)
+#print(tut)
+#print(thing3)
+#print(tun)
 
 zero=('The Fool')
 a=('I The Magician')
@@ -80,9 +371,13 @@ nnnnn=('Knight of Cups')
 
 tarot0=[zero,a,l,d,p,g,s,u,cc,ff,ii,ll,aaa,ddd,ggg,kkk,nnn,aaaa,dddd,gggg,jjjj,mmmm,bbbbb,fffff,iiiii,lllll]
 
-tarot10=[j,k,c,o,f,r,i,aa,dd,gg,jj,mm,bbb,eee,hhh,lll,bbbb,eeee,hhhh,kkkk,nnnn,ccccc,ggggg,jjjjj,mmmmm]
+t0=len(tarot0)
 
-tarot21=[v,b,m,e,q,t,bb,ee,hh,kk,nn,ccc,fff,iii,mmm,cccc,ffff,iiii,llll,aaaaa,ddddd,eeeee,hhhhh,kkkkk,nnnnn]
+tarot10=[j,k,c,o,f,r,i,aa,dd,gg,jj,mm,bbb,eee,hhh,lll,jjj,bbbb,eeee,hhhh,kkkk,nnnn,ccccc,ggggg,jjjjj,mmmmm]
+t10=len(tarot10)
+
+tarot21=[v,b,m,e,q,t,h,bb,ee,hh,kk,nn,ccc,fff,iii,mmm,cccc,ffff,iiii,llll,aaaaa,ddddd,eeeee,hhhhh,kkkkk,nnnnn]
+t21=len(tarot21)
 
 import random
 
@@ -120,8 +415,45 @@ elif sect3==2:
 elif sect3==3:
    deck3=tarot21
 
+count=0
+
+while count<=tus:
+    card1=deck1[count]
+    if count==25:
+        count=0
+        tus=tus-26 
+    else:
+        count=count+1
+ 
+print (card1)
+
+count=0
+
+while count<=tut:
+    card2=deck2[count]
+    if count==25:
+        count=0
+        tut=tut-26 
+    else:
+        count=count+1
+        
+print (card2)
+
+count=0
+
+while count<=tun:
+    card3=deck3[count]
+    if count==25:
+        count=0
+        tun=tun-26 
+    else:
+        count=count+1
+        
+print (card3)
+
 
  
-card1=deck1[8]
 
-print (card1)
+#print (t0)
+#print (t10)
+#print (t21)
